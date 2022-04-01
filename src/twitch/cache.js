9a7dd19b33d70a4a6754c7e6a5f2ad5090edb88b
mrmugame <mr.mugame@gmail.com>
2022-04-01 23:15:11 +0200

    remove comments

--- src/twitch/cache.js
+++ src/twitch/cache.js
@@ -19,37 +19,3 @@ export const load = async () => {
 export const save = (data) => {
     chrome.storage.local.set({data_cache: data});
 }
-
-
-// class DataCache {
-
-//     constructor() {
-//         this.age = new Date();
-//         this.data = {
-//             streams: null,
-//             user: null
-//         }
-//     }
-
-//     async load() {
-//         return new Promise((resolve, reject) => chrome.storage.local.get(["data_cache"], res => {
-//             console.log(res.data_cache);
-//             if (res.streams == undefined || res.user == undefined) {
-//                 reject();
-//             }
-//             this.data = res.data_cache;
-//             resolve();
-//         }))
-//     }
-
-//     save() {
-//         return new Promise((resolve, reject) => chrome.storage.local.set({data_cache: this.data}))
-//     }
-
-//     isValid(minutes) {
-//         const now = new Date();
-//         const time_diff = now.getTime() - this.age.getTime();
-//         return time_diff > (minutes * 60 * 1000)
-//     }
-
-// }
