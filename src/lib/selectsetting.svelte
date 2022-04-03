3987f0e0a057d9406f2f78bf0fd3095386b83c2d
mrmugame <mr.mugame@gmail.com>
2022-04-03 22:09:54 +0200

    select options

---
+++ src/lib/selectsetting.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let selected;
+    export let options;
+    export let name;
+</script>
+
+
+
+<div class="flex items-center my-2">
+    <h3 class="text-base font-roboto text-strongtext font-semibold">{name}</h3>
+    <select bind:value={selected} class="bg-twitch text-lighttext mx-5 rounded-md h-5">
+        {#each options as option}
+            <option value={option}>
+                {option}min
+            </option>
+        {/each}
+    </select>
+</div>
