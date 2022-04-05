76d07cca442ed043ed607d95e83c38ab9200c904
mrmugame <mr.mugame@gmail.com>
2022-04-05 23:34:16 +0200

    added promise to event listener to not throw error

--- src/background.js
+++ src/background.js
@@ -8,19 +8,22 @@ const CLIENTID = "i8uqx7hag4dcu1ipxqeggxyn1ys3om";
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.data != "OAUTH") { return }
 
-    chrome.tabs.create({ url: makeURL() }, tab => {
-        chrome.tabs.onUpdated.addListener((tabId, info) => {
-            if (info.status === 'complete' && tabId === tab.id) {
-                chrome.tabs.query({active: true, currentWindow: true}, ([newtab]) => {
-                    let url = new URL(newtab.url);
-                    let parameters = new URLSearchParams(url.hash);
-
-                    if (parameters.get("#access_token") != null) {
-                        chrome.storage.local.set({access_token: parameters.get("#access_token")});
-                        chrome.tabs.remove(newtab.id);
-                    }
-                })
-            }
+    return new Promise(resolve => {
+        chrome.tabs.create({ url: makeURL() }, tab => {
+            chrome.tabs.onUpdated.addListener((tabId, info) => {
+                if (info.status === 'complete' && tabId === tab.id) {
+                    chrome.tabs.query({active: true, currentWindow: true}, ([newtab]) => {
+                        let url = new URL(newtab.url);
+                        let parameters = new URLSearchParams(url.hash);
+    
+                        if (parameters.get("#access_token") != null) {
+                            chrome.storage.local.set({access_token: parameters.get("#access_token")});
+                            chrome.tabs.remove(newtab.id);
+                            resolve();
+                        }
+                    })
+                }
+            });
         });
     });
 })
