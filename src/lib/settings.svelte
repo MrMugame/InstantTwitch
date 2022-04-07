3818c71a45e3943c4b56dbd55c8d6aa38b587241
mrmugame <mr.mugame@gmail.com>
2022-04-07 14:36:52 +0200

    fixed colors for lightmode

--- src/lib/settings.svelte
+++ src/lib/settings.svelte
@@ -31,7 +31,7 @@
 
 <div class="flex flex-col p-2 ">
     <div class="flex justify-between mb-5">
-        <svg on:click={_ => page.set("streams")} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover: dark:stroke-lighttext text-lightlighttext hover:stroke-strongtext" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <svg on:click={_ => page.set("streams")} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover: dark:stroke-lighttext stroke-lightstrongtext dark:hover:stroke-strongtext hover:stroke-lightlighttext" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
         </svg>
         <h2 class="font-roboto dark:text-lighttext text-lightlighttext font-semibold text-base mx-6">Settings</h2>
--- src/lib/stream.svelte
+++ src/lib/stream.svelte
@@ -22,14 +22,14 @@
     }
 </script>
 
-<div on:click={openTab} class="w-full h-20 flex  p-3 border-b-[1px] border-lightborder hover:bg-twitch cursor-pointer overflow-hidden transition-colors">
+<div on:click={openTab} class="w-full h-20 flex  p-3 border-b-[1px] dark:text-lighttext text-lightlighttext hover:text-lighttext border-lightborder hover:bg-twitch cursor-pointer overflow-hidden transition-colors">
     <div class="relative h-full">
         <img class="h-full aspect-video max-w-none" src={parseThumbnailUrl(stream.thumbnail_url)} alt="thumbnail">
         <p class="absolute bottom-0 right-0 px-[1px] bg-black bg-opacity-60 text-strongtext text-xs">{getTime()}</p>
     </div>
     <div class="ml-3">
-        <h1 class="text-xl dark:text-strongtext text-lightstrongtext bg-opacity-5leading-5 font-roboto font-bold mb-1">{stream.user_name}</h1>
-        <h2 class="font-roboto dark:text-lighttext text-lightlighttext text-xs">{stream.game_name} - {stream.viewer_count} Viewer</h2>
-        <h2 class="font-roboto dark:text-lighttext text-lightlighttext w-[300px] overflow-ellipsis whitespace-nowrap overflow-hidden inline-block text-xs">{stream.title}</h2>
+        <h1 class="text-xl dark:text-strongtext text-lightstrongtext bg-opacity-5 leading-5 font-roboto font-bold mb-1">{stream.user_name}</h1>
+        <h2 class="font-roboto text-xs">{stream.game_name} - {stream.viewer_count} Viewer</h2>
+        <h2 class="font-roboto w-[300px] overflow-ellipsis whitespace-nowrap overflow-hidden inline-block text-xs">{stream.title}</h2>
     </div>
 </div>
